58ec5baf9876c6ce9839694e0d896dbb2b142e7d
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-11-04 16:04:38 -0300

    Update moedas.py

--- moedas.py
+++ moedas.py
@@ -1,19 +1,26 @@
-from cs50 import get_string
 from cs50 import get_float
+from sys import argv
 
-cash = get_float("Moedas: ")
-
-while True:
-    if cash >= 0:
-
-       cents = int(cash * 100)
-
-       c25 = int(cents / 25)
-       c10 = int((cents - (c25 * 25)) / 10)
-       c5 = int((cents - (c25 * 25) - (c10 * 10)) / 5)
-       c1 = int(cents - (c25 * 25) - (c10 * 10) - (c5 * 5))
-       print(c25 + c10 + c5 + c1)
-
-       break
-    else:
-        cash = get_float("Moedas: ")
+#Informar valor da compra, recebido, troco em 25, 10, 5 e 1 centavo.
+compra = get_float ("Valor da compra: ")
+recebido = get_float ("Valor recebido: ")
+troco = recebido - compra
+moedas = [{'25': 0}, {'10': 0}, {'5': 0}, {'1': 0}]
+if troco > 0.25:
+    troco - 0.25
+    (moedas[0]) =+1
+elif troco > 0.10:
+    troco - 0.10
+    (moedas[1]) += 1
+elif troco > 0.05:
+    troco - 0.05
+    (moedas[2]) += 1
+elif troco > 0.01:
+    troco - 0.01
+    (moedas[3]) += 1
+print("Troco: R$ ", troco)
+moeda = (moedas[0])
+def f(moeda):
+    return (moedas[moeda])
+for i in range(0, (moeda+3), 1):
+    print(f"{(moeda[i])} moedas de: {(moedas[i])}")
