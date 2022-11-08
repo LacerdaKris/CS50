64d9bb34c74cf3c949614c1e4a308e7ec06e65ef
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-11-08 16:40:41 -0300

    Update moedas.py

--- moedas.py
+++ moedas.py
@@ -1,26 +1,24 @@
 from cs50 import get_float
-from sys import argv
 
 #Informar valor da compra, recebido, troco em 25, 10, 5 e 1 centavo.
 compra = get_float ("Valor da compra: ")
 recebido = get_float ("Valor recebido: ")
 troco = recebido - compra
-moedas = [{'25': 0}, {'10': 0}, {'5': 0}, {'1': 0}]
-if troco > 0.25:
-    troco - 0.25
-    (moedas[0]) =+1
-elif troco > 0.10:
-    troco - 0.10
-    (moedas[1]) += 1
-elif troco > 0.05:
-    troco - 0.05
-    (moedas[2]) += 1
-elif troco > 0.01:
-    troco - 0.01
-    (moedas[3]) += 1
-print("Troco: R$ ", troco)
-moeda = (moedas[0])
-def f(moeda):
-    return (moedas[moeda])
-for i in range(0, (moeda+3), 1):
-    print(f"{(moeda[i])} moedas de: {(moedas[i])}")
+print(F"Troco: R$ {troco:.2f}")
+moedas = {'25':0, '10':0, '5':0, '1':0}
+for c in moedas:
+    if troco > 0.25:
+        troco -= 0.25
+        moedas['25'] = moedas.get('25', c) +1
+    elif troco > 0.10:
+        troco -= 0.10
+        moedas['10'] = moedas.get('10', c) +1
+    elif troco > 0.05:
+        troco -= 0.05
+        moedas['5'] = moedas.get('5', c) +1
+    elif troco > 0.01:
+        troco -= 0.01
+        moedas['1'] = moedas.get('1', c) +1
+
+for i in moedas:
+    print(f"Sendo: {moedas.get(i)} moeda(s) de {i}")
