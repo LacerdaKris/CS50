2850cb5c236efaf3b0f6e4d7401b303819ee2207
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-11-21 17:51:51 -0300

    Update cartaocredito.py
    De acordo com o algoritmo de Luhn, você pode determinar se um número de cartão de crédito é (sintaticamente) válido da seguinte maneira:
    1 - Multiplique cada segundo digito por 2, começando com o penúltimo dígito do número e, em seguida, some os dígitos desses produtos.
    2 - Adicione essa soma à soma dos dígitos que não foram multiplicados por 2.
    3 - Se o último dígito do total for 0 (ou, mais formalmente, se o módulo total 10 for congruente com 0), o número é válido!

--- cartaocredito.py
+++ cartaocredito.py
@@ -1,6 +1,6 @@
-#AMERICAN 15 DIGITOS, INICIO 34 OU 37 - 348282246310005 - 378282246310005
-#VISA 13 OU 16 DIGITOS, INICIO 4 - 4312888888881881
-#MASTER 16 DIGITOS, INICIO 51, 52, 53, 54 OU 55 - 5405105105105100
+#AMERICAN 15 DIGITOS, INICIO 34 OU 37 - i: 348282246310005 - v: 378282246310005
+#VISA 13 OU 16 DIGITOS, INICIO 4 - i: 4312888888881881 - v: 4003600000000014
+#MASTER 16 DIGITOS, INICIO 51, 52, 53, 54 OU 55 - i: 5405105105105100 - v: 5400105005105020
 #USAR Algoritmo de Luhn para verificação de validade
 from cs50 import get_string
 import math
@@ -11,7 +11,7 @@ def get_sum(n): # Função para somar os dígitos de um número
     return sum
 def main():
     cartao = get_string("Numero do cartao: ")
-    digitos = [x for x in str(cartao)]
+    digitos = [int(x) for x in str(cartao)]
     digito = [int(x) for x in str(cartao)]
     lista1 = []
     lista2 = []
@@ -30,9 +30,8 @@ def main():
         lista2[i]=get_sum(lista2[i])
     total=sum(lista1+lista2)
     ult=str(total)[-1] #último dígito do resultado
-    print(ult)
 #conferir se quantidade de digitos é válida e se o último do total da soma (algoritmo de luhn) é zero, se sim, qual bandeira:
-    if 16 < len(cartao) > 13 or len(cartao) == 14 or ult != 0:
+    if 16 < len(cartao) > 13 or len(cartao) == 14 or int(ult) > 0:
         print("INVALID")
     elif digitos[0] == 3 and (digitos[1] == 4 or 7):
         print("AMEX")
