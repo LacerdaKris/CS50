044444d0b463b929fd4f84e861e89a4854bd1984
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-11-22 15:38:56 -0300

    Update legibilidade.py
    O índice Coleman-Liau de um texto é projetado para mostrar qual nível escolar (EUA) é necessário para entender o texto. A fórmula é:
    índice = 0,0588 * L - 0,296 * S - 15,8
    Aqui, L é o número médio de letras por 100 palavras no texto e S é o número médio de sentenças por 100 palavras no texto.

--- legibilidade.py
+++ legibilidade.py
@@ -1,26 +1,27 @@
+#índice = 0,0588 * L - 0,296 * S - 15,8
+#sendo L a média de letras por 100 palavras e S a média de sentenças por 100 palavras no texto.
 from cs50 import get_string
+import re
 
-text = get_string("Insira seu texto: ")
+texto = get_string("Digite o texto: ")
+#retirar pontuações do texto
+res = re.sub(r'[^\w\s]', '', texto)  
+palavras = 0
+letras = 0
+#contar palavras e letras do texto sem pontuação
+for i in res.split():
+    palavras += 1
+    letras = len(i)+letras
+#contar sentenças
+sentenças = texto.count("?") + texto.count(".") + texto.count("!")
+#aplicar fórmula de Coleman-Liau
+L = (letras/palavras)*100
+S = (sentenças/palavras)*100
+nivel = round(0.0588*L-0.296*S-15.8)
 
-letters = 0
-words = 1
-sentences = 0
-
-for char in text:
-  if char.isalpha():
-    letters += 1
-  if char.isspace():
-    words += 1
-  if char == '.' or char == '!' or char == '?':
-    sentences += 1
-
-l = letters * 100 / words
-s = sentences * 100 / words
-index = 0.0588 * l - 0.296 * s - 15.8
-
-if index < 1:
-  print("Antes da Grade 1")
-elif index > 16:
-  print("Grade 16+")
-else:
-  print(f"Grade {int(index)}")
+if 1 <= nivel <= 16:
+    print(f"Nível: {nivel}")
+elif nivel < 1:
+    print("Nível: Menor que 1")
+elif nivel > 16:
+    print("Nível: 16+")
