99d7a193e643ec2e7702886c62333636d231ed8d
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-11-22 15:49:01 -0300

    Update legibilidade.py

--- legibilidade.py
+++ legibilidade.py
@@ -1,4 +1,4 @@
-#índice = 0,0588 * L - 0,296 * S - 15,8
+#fórmula de Coleman-Liau = 0,0588 * L - 0,296 * S - 15,8
 #sendo L a média de letras por 100 palavras e S a média de sentenças por 100 palavras no texto.
 from cs50 import get_string
 import re
@@ -18,10 +18,23 @@ sentenças = texto.count("?") + texto.count(".") + texto.count("!")
 L = (letras/palavras)*100
 S = (sentenças/palavras)*100
 nivel = round(0.0588*L-0.296*S-15.8)
-
+#imprimir nível em 3 opções conf. instrução do exercício
 if 1 <= nivel <= 16:
     print(f"Nível: {nivel}")
 elif nivel < 1:
     print("Nível: Menor que 1")
 elif nivel > 16:
     print("Nível: 16+")
+    
+#a contagem das letras, palavras e sentenças do texto também poderia ser feita conforme abaixo:
+letters = 0
+words = 1
+sentences = 0
+
+for char in texto:
+  if char.isalpha():
+    letters += 1
+  if char.isspace():
+    words += 1
+  if char == '.' or char == '!' or char == '?':
+    sentences += 1
