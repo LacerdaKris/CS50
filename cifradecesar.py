0a1d10f66ca9746804ef79d745f0543294c12a9b
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-11-23 17:49:57 -0300

    Update cifradecesar.py

--- cifradecesar.py
+++ cifradecesar.py
@@ -6,10 +6,16 @@ from cs50 import get_int
 
 chave = get_int("Digite a chave (número): ")
 texto = get_string("Digite o texto: ")
-cifrado = []
-
-for l in texto:
-    letras = (int(l) + chave)% 26
-    cifrado.append(letras)
+digitos = []
+caracter = range(len(texto)+1)
+        
+for i in caracter:
+#converte letras e números no decimal ascii com a chave somada e adicona a lista digitos
+    if 123 > i > 64 or 58 > i > 47:
+        ascii = ord(texto[i])+chave
+        digitos.append(ascii)
+#traduz pontuacoes em decimal ascii e adiciona a lista digitos
+    else:
+        digitos.append(i)
     
-print(cifrado)
+    print(chr(digitos[i]))
