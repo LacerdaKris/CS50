72d813fbbff5cd438b199967f5d3a8e10b5ac394
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-12-01 17:12:27 -0300

    Create substituição.py

---
+++ substituição.py
@@ -0,0 +1,28 @@
+#criptografa usando uma cifra de substituição, com 26 caracteres do alfabeto como chave.
+#exemplo: NQXPOMAFTRHLZGECYJIUWSKDVB - significa que A converte em N, B em Q, etc...
+from cs50 import get_string
+import string
+
+chave = get_string("Digite a chave com 26 letras: ")
+while len(chave) != 26:
+    chave = get_string("Digite a chave com 26 letras: ")
+digitos = [x for x in str(chave)]
+#faz uma lista com as letras do alfabeto:
+alfabeto = list(string.ascii_lowercase)
+dicionario = {}
+n = 0
+#inclui ao dicionário cada letra do alfabeto com sua chave correspondente
+for n in range (26):
+    dicionario[alfabeto[n]] = digitos[n]
+    n += 1
+
+texto = get_string("Digite o texto: ")
+cifrado = []
+for d in texto:
+    if d in alfabeto:
+        cifrado.append()
+#se forem pontuações ou espaços adiciona a lista cifrado (sem alteração)
+    else:
+        cifrado.append(chr(j))
+#"*" imprime todos os itens da lista, e "sep" os separa pelo que for colocado entre aspas.
+print("Texto cifrado: ", *cifrado, sep="", end="")
